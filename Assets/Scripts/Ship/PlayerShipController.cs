9a6e91785e8065d8fb58e624f16921e90c97297c
Mitchell Philipp <mgpalpha1@gmail.com>
2023-03-14 17:46:52 -0400

    refactor to separate ship control from movement

---
+++ Assets/Scripts/Ship/PlayerShipController.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(ShipCore))]
+public class PlayerShipController : MonoBehaviour
+{
+
+    private ShipCore shipCore;
+
+    private float throttle;
+    [SerializeField] private float throttleSpeed = 1;
+
+    private Vector2 turnInput;
+    [SerializeField] private float turnSensitivity = 5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        shipCore = GetComponent<ShipCore>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W)) {
+            throttle = Mathf.MoveTowards(throttle, 1, throttleSpeed * Time.deltaTime);
+        } else if (Input.GetKey(KeyCode.S)) {
+            throttle = Mathf.MoveTowards(throttle, 0, throttleSpeed * Time.deltaTime);
+        }
+
+        shipCore.SetThrottle(throttle);
+
+        Vector2 newTurnInput = new Vector2(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y")) * turnSensitivity *.01f;
+        float aspRatio = Screen.width / Screen.height;
+        newTurnInput.y /= aspRatio;
+        turnInput += newTurnInput;
+
+        turnInput = Vector2.ClampMagnitude(turnInput, 1);
+
+        shipCore.SetTurning(turnInput);
+    }
+}
--- Assets/Scripts/Ship/ShipCore.cs
+++ Assets/Scripts/Ship/ShipCore.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ShipControllerCore : MonoBehaviour
+public class ShipCore : MonoBehaviour
 {
 
     private Rigidbody _rb;
@@ -10,12 +10,10 @@ public class ShipControllerCore : MonoBehaviour
     private float throttle = 0;
     private float currentSpeed = 0;
     private Vector2 turnInput = Vector2.zero;
-    [SerializeField] private float throttleSpeed = 1;
     [SerializeField] private float baseMaxSpeed = 25;
     [SerializeField] private float baseAcceleration = 10;
     [SerializeField] private float baseBrake = 6;
 
-    [SerializeField] private float turnSensitivity = 1f;
     [SerializeField] private float baseTurnSpeed = 1f;
     [SerializeField] private float turnSpeedDampingFactor = .4f;
     
@@ -29,20 +27,24 @@ public class ShipControllerCore : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.W)) {
-            throttle = Mathf.MoveTowards(throttle, 1, throttleSpeed * Time.deltaTime);
-        } else if (Input.GetKey(KeyCode.S)) {
-            throttle = Mathf.MoveTowards(throttle, 0, throttleSpeed * Time.deltaTime);
-        }
+        
 
-        Vector2 newTurnInput = new Vector2(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y")) * turnSensitivity;
-        float aspRatio = Screen.width / Screen.height;
-        newTurnInput.y /= aspRatio;
-        turnInput += newTurnInput;
+        // Vector2 newTurnInput = new Vector2(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y")) * turnSensitivity;
+        // float aspRatio = Screen.width / Screen.height;
+        // newTurnInput.y /= aspRatio;
+        // turnInput += newTurnInput;
 
-        turnInput = Vector2.ClampMagnitude(turnInput, 1);
+        // turnInput = Vector2.ClampMagnitude(turnInput, 1);
 
 
+    }
+
+    public void SetThrottle(float val) {
+        throttle = val;
+    }
+
+    public void SetTurning(Vector2 val) {
+        turnInput = val;
     }
 
     /// <summary>
@@ -60,7 +62,7 @@ public class ShipControllerCore : MonoBehaviour
         float turnDamping = Mathf.Lerp(1, turnSpeedDampingFactor, Mathf.InverseLerp(0, baseMaxSpeed, currentSpeed));
 
         Vector2 smoothedTurning = turnInput * Mathf.Pow(turnInput.magnitude, 2);
-        transform.Rotate(new Vector3(smoothedTurning.y, smoothedTurning.x, 0) * baseTurnSpeed * turnDamping * Time.fixedDeltaTime, Space.Self);
+        transform.Rotate(new Vector3(-smoothedTurning.y, smoothedTurning.x, 0) * baseTurnSpeed * turnDamping * Time.fixedDeltaTime, Space.Self);
 
         transform.position += transform.forward * currentSpeed * Time.fixedDeltaTime;
     }
